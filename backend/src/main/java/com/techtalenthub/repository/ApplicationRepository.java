8fc97eadd718321957645679c7645859a725b92c
Gabriel Soares <123321309+gabrielmendezsoares@users.noreply.github.com>
2024-09-03 06:20:20 -0300

    Delete backend directory

--- backend/src/main/java/com/techtalenthub/repository/ApplicationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.techtalenthub.repository;
-
-import com.techtalenthub.model.Application;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApplicationRepository extends JpaRepository<Application, Long> {
-}
--- backend/src/main/java/com/techtalenthub/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.techtalenthub.repository;
-
-import java.util.Optional;
-import com.techtalenthub.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-  Optional<User> findByEmail(String email);
-}
